19ca3b9a1512b2c91ac00eac42d2494d22036e9f
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-05-12 21:16:55 -0300

    Update leet013.py

--- leet013.py
+++ leet013.py
@@ -1,14 +1,24 @@
 def romanToInt(s):
-    rti = [["I", 1],
-           ["V", 5],
-           ["X", 10],
-           ["L", 50],
-           ["C", 100],
-           ["D", 500],
-           ["M", 1000]]
+    x = 0
+    rti = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     s = list(s)
     for i in range(len(s)):
-        pass
+        if i == len(s) - 1:
+            x += rti[s[i]]
+        else:
+            if rti[s[i]] < rti[s[i + 1]]:
+                x -= rti[s[i]]
+            else:
+                x += rti[s[i]]
+    return x
 
 
 print(romanToInt("III"))
