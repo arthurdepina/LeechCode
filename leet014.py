bf5113735d33bfa0c32f77c94b6f24c37380ac58
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-06-28 06:11:54 -0300

    starting exercise 14, and creating tests.py

---
+++ leet014.py
@@ -0,0 +1,11 @@
+def longestCommonPrefix(strs):
+    """
+    Write a function to find the longest common prefix string amongst an array of strings.
+    If there's no common prefix, return an empty string ""
+    """
+
+
+print(longestCommonPrefix(["flower", "flow", "flight"]))  # Should return "fl"
+print(longestCommonPrefix(["dog", "racecar", "car"]))  # Should return ""
+print(longestCommonPrefix(["lag", "staggering", "faggot"]))  # Should return "ag"
+
