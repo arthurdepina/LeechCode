a9a8cd31fdc6800c53498e33ccc13491154cb6fb
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-06-30 06:41:33 -0300

    Update leet014.py
    Eu acabo de perceber que sou retardado.
    O exercício pede o PREFIXO em comum, não qualquer parte da palavra.
    gah fucking damnit

--- leet014.py
+++ leet014.py
@@ -15,7 +15,11 @@ def longestCommonPrefix(strs):
     # de trás para frente (<-) e outro que remove da frente para trás (->); a cada iteração
     # comparando com as outras palavras do array, para encontrar uma repetição
 
+    finder = False
     found = ""
+    found_a = ""
+    found_b = ""
+    found_c = ""
 
     smallest_a = smallest
     smallest_b = smallest
@@ -23,34 +27,50 @@ def longestCommonPrefix(strs):
 
     for i in smallest_a:
         for s in strs:
-            if smallest_a in s:
-                found = smallest_a
-            else:
-                found = ""
+            if smallest_a in s and len(smallest_a) > len(found_a):
+                finder = True
+                found_a = smallest_a
+            elif smallest_a not in s:
+                found_a = ""
+                finder = False
         smallest_a = smallest_a[:-1]
+
+    if finder:
+        found = found_a
 
     for j in smallest_b:
         for s in strs:
-            if smallest_b in s and len(smallest_b) > len(found):
-                found = smallest_b
-            else:
-                found = ""
+            if smallest_b in s and len(smallest_b) > len(found_b):
+                finder = True
+                found_b = smallest_b
+            elif smallest_b not in s:
+                found_b = ""
+                finder = False
         smallest_b = smallest_b[1:]
+
+    if finder:
+        found = found_b
 
     for k in smallest_c:
         for s in strs:
-            if smallest_c in s and len(smallest_c) > len(found):
-                found = smallest_c
-            else:
-                found = ""
+            if smallest_c in s and len(smallest_c) > len(found_c):
+                finder = True
+                found_c = smallest_c
+            elif smallest_c not in s:
+                found_c = ""
+                finder = False
         smallest_c = smallest_c[:-1]
         smallest_c = smallest_c[1:]
+
+    if finder:
+        found = found_c
 
     return found
 
 
+"""
 print(longestCommonPrefix(["flower", "flow", "flight"]))  # Should return "fl"
-"""
 print(longestCommonPrefix(["dog", "racecar", "car"]))  # Should return ""
-print(longestCommonPrefix(["lag", "staggering", "faggot"]))  # Should return "ag"
 """
+print(longestCommonPrefix(["lag", "staggering", "faggot"]))  # Should return "ag"
+
