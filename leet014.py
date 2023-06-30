a94ae8ec0a9c8e1fdb7df1b18ce00ba132440ccf
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-06-30 05:53:40 -0300

    Trying it out with three loops.

--- leet014.py
+++ leet014.py
@@ -15,31 +15,42 @@ def longestCommonPrefix(strs):
     # de trás para frente (<-) e outro que remove da frente para trás (->); a cada iteração
     # comparando com as outras palavras do array, para encontrar uma repetição
 
-    found = False
+    found = ""
 
-    for i in range(len(smallest)//2 - 1):
-        for word in strs:
-            if smallest in word:
-                found = True
+    smallest_a = smallest
+    smallest_b = smallest
+    smallest_c = smallest
+
+    for i in smallest_a:
+        for s in strs:
+            if smallest_a in s:
+                found = smallest_a
             else:
-                found = False
-        if found:
-            return smallest
-        else:
-            smallest = smallest[:-1]
-        for j in range(len(smallest)//2):
-            for word in strs:
-                if smallest in word:
-                    found = True
-                else:
-                    found = False
-            if found:
-                return smallest
+                found = ""
+        smallest_a = smallest_a[:-1]
+
+    for j in smallest_b:
+        for s in strs:
+            if smallest_b in s and len(smallest_b) > len(found):
+                found = smallest_b
             else:
-                smallest = smallest[1:]
+                found = ""
+        smallest_b = smallest_b[1:]
+
+    for k in smallest_c:
+        for s in strs:
+            if smallest_c in s and len(smallest_c) > len(found):
+                found = smallest_c
+            else:
+                found = ""
+        smallest_c = smallest_c[:-1]
+        smallest_c = smallest_c[1:]
+
+    return found
 
 
 print(longestCommonPrefix(["flower", "flow", "flight"]))  # Should return "fl"
+"""
 print(longestCommonPrefix(["dog", "racecar", "car"]))  # Should return ""
 print(longestCommonPrefix(["lag", "staggering", "faggot"]))  # Should return "ag"
-
+"""
--- tests.py
+++ tests.py
@@ -1,4 +1,17 @@
 a = "staggering"
-a = a[:-1]
-print(a)
+b = a
+c = a
+
+for i in a:
+    print(a)
+    a = a[:-1]
+
+for j in b:
+    print(b)
+    b = b[1:]
+
+for k in c:
+    print(c)
+    c = c[:-1]
+    c = c[1:]
 
