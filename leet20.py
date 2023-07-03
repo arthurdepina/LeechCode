dc878ea9cc8dbd3c018fd064baf59cc3ec19718b
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-07-03 08:14:14 -0300

    Update leet20.py

--- leet20.py
+++ leet20.py
@@ -12,7 +12,7 @@ def isValid(s):
     close_curly_brackets = False
 
     for character in s:
-        if character == "(":
+        if character == "(":  # THE OPENINGS
             if close_parenthesis:
                 return "false"
             open_parenthesis = True
@@ -24,16 +24,28 @@ def isValid(s):
             if close_curly_brackets:
                 return "false"
             open_curly_brackets = True
-        elif character == ")":
+        elif character == ")":  # THE CLOSINGS
             if not open_parenthesis:
+                return "false"
+            if open_square_brackets and not close_square_brackets:
+                return "false"
+            if open_curly_brackets and not close_curly_brackets:
                 return "false"
             close_parenthesis = True
         elif character == "]":
             if not open_square_brackets:
                 return "false"
+            if open_parenthesis and not close_parenthesis:
+                return "false"
+            if open_curly_brackets and not close_curly_brackets:
+                return "false"
             close_square_brackets = True
         elif character == "}":
             if not open_curly_brackets:
+                return "false"
+            if open_parenthesis and not close_parenthesis:
+                return "false"
+            if open_square_brackets and not close_square_brackets:
                 return "false"
             close_curly_brackets = True
 
@@ -43,3 +55,5 @@ def isValid(s):
 print(isValid("()"))      # Should print "true"
 print(isValid("()[]{}"))  # Should print "true"
 print(isValid("(]"))      # Should print "false"
+print(isValid("([)]"))    # Should print "false"
+print(isValid("{[]}"))    # Should print "true"
