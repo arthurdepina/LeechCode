dd14cc1dc8b70051c62c4adff74b94617266e2f6
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-07-26 23:48:56 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-def mergeTwoLists(list1, list2):
-    output = list()
-    for i in list1:
-        output.append(i)
-    for j in list2:
-        output.append(j)
-    output.sort()
-    return output
+class Solution(object):
 
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        tail = dummy
 
-lista_a = [1, 2, 4]
-lista_b = [1, 3, 4]
-print(mergeTwoLists(lista_a, lista_b))
+        while list1 and list2:  # while they're not empty
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        # At the end of the while loop above, at least one of the lists will be empty
+        if list1:  # If list1 is not empty
+            tail.next = list1
+        elif list2:  # If list2 is not empty
+            tail.next = list2
+
+        return dummy.next
