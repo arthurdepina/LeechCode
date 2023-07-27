cf40de5022d0cdf0962d2f0832fc6aa342fb8d58
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-07-26 23:49:36 -0300

    a

--- leet021.py
+++ leet021.py
@@ -4,21 +4,24 @@
 #         self.val = val
 #         self.next = next
 
-def mergeTwoLists(list1, list2):
-    if len(list1) > len(list2):
-        upper_bound = len(list1)
-    else:
-        upper_bound = len(list2)
-    k = 0
-    i = 0
-    j = 0
-    output = list()
-    while k < upper_bound:
-        if i == len(list1) or j == len(list2):
-            break
-        if list1[i] < list2[j]:
-            output[k] = list[i]
-        else:
-            output[k] = list[j]
-        k += 1
+class Solution(object):
 
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:  # while they're not empty
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        # At the end of the while loop above, at least one of the lists will be empty
+        if list1:  # If list1 is not empty
+            tail.next = list1
+        elif list2:  # If list2 is not empty
+            tail.next = list2
+
+        return dummy.next
