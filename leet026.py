42d25af1ee35f63a63344cc8dec8332e2c7f6acc
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-11 21:53:40 -0300

    Update leet026.py

--- leet026.py
+++ leet026.py
@@ -3,18 +3,17 @@
 """
 
 def removeDuplicates(nums) -> int:
-    index = 1
-    k = len(nums)
-    while index <= k - 1:
-        if nums[index] == nums[index - 1]:
-            del nums[index]
-            k -= 1
-        else:
-            index += 1
-    print(nums)
-    return k
-
+    j = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[j] = nums[i]
+            j += 1
+    return j
  
 
 # print(removeDuplicates([1,1,2])) # returns 2 -- nums will be [1, 2]
 print(removeDuplicates([0,0,1,1,1,2,2,3,3,4])) # returns 5 -- nums will be [0,1,2,3,4,_,_,_,_,_]
+                                               # in this particular solution,
+                                               # nums = [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
+                                               # in the previous commited solution:
+                                               # nums = [0, 1, 2, 3, 4]
