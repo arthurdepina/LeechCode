e2a3cf4e3983042296b81ec2502c8510278f7cce
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-13 01:43:17 -0300

    Update leet009.py

--- leet009.py
+++ leet009.py
@@ -1,12 +1,23 @@
-def isPalindrome(x):
-    x = str(x)
-    for i in range(len(x)):
-        if x[i] == x[len(x) - i - 1]:
-            continue
-        else:
-            return False
-    return True
+# 09. Palindrome Number
+
+def isPalindrome(x) -> bool:
+    if x < 0 or x % 10 == 0: return False
+    backwards = 0
+    while x > backwards:
+        backwards = backwards * 10 + x % 10
+        x //= 10
+    return (x == backwards or x == backwards//10)
 
 
-torf = isPalindrome(1234554321)
+torf = isPalindrome(1221)
 print(torf)
+
+
+# def isPalindrome(x):
+#     x = str(x)
+#     for i in range(len(x)):
+#         if x[i] == x[len(x) - i - 1]:
+#             continue
+#         else:
+#             return False
+#     return True
