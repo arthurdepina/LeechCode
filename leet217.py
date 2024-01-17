3fcb8b0900a490f998ce03ba8e5858ad62b4cd4e
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-16 22:32:48 -0300

    Update leet217.py

--- leet217.py
+++ leet217.py
@@ -1,10 +1,9 @@
 # 217. Contains Duplicate
 
 def containsDuplicate(nums) -> bool:
-    mylist = list(dict.fromkeys(nums))
-    return mylist != nums
+    return list(dict.fromkeys(nums)) != nums
 
 
-# print(containsDuplicate([1,2,3,1]))
-# print(containsDuplicate([1,2,3,4]))
-# print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+print(containsDuplicate([1,2,3,1]))
+print(containsDuplicate([1,2,3,4]))
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
