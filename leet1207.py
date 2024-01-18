c99367df89b89ad6ed37891365de18576fd9ae74
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-18 03:03:37 -0300

    update 1207
    now using sets

--- leet1207.py
+++ leet1207.py
@@ -5,7 +5,8 @@ def uniqueOccurrences(arr) -> bool:
     for i in arr:
         if i in occurrences: occurrences[i] += 1
         else: occurrences[i] = 1
-    return list(dict.fromkeys(occurrences.values())) == [i for i in occurrences.values()]     
+    occ = list(occurrences.values())
+    return len(set(occ)) == len(occ) 
 
 
 print(uniqueOccurrences([1,2,2,1,1,3])) # true
