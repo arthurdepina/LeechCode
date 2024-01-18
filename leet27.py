fcaef7ed33bc365873c6f4abe8978e6dc787f157
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-18 03:35:37 -0300

    Update leet27.py

--- leet27.py
+++ leet27.py
@@ -12,3 +12,18 @@ def removeElement(nums, val) -> int:
 
 print(removeElement([3,2,2,3], 3)) # 2, nums = [2,2,_,_]
 print(removeElement([0,1,2,2,3,0,4,2], 2)) # 5, nums = [0,1,4,0,3,_,_,_]
+print(removeElement([2, 2, 2, 1], 2))
+
+
+# The following solution also works, and I did manage
+# to get a 94% with it; but leetcode runtime measure
+# is not very reliable. I do think this solution is
+# nicer, so I'll keep it down here.
+
+# def removeElement(nums, val) -> int:
+#     index = 0
+#     for i in range(len(nums)):
+#         if nums[i] != val:
+#             nums[index] = nums[i]
+#             index += 1
+#     return index
