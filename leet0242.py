cec9e9fdf2f3dc4577fa02d8f5b71cb41c5170e6
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-19 01:13:22 -0300

    Update leet0242.py

--- leet0242.py
+++ leet0242.py
@@ -8,3 +8,25 @@ def isAnagram(s, t) -> bool:
 
 print(isAnagram("anagram", "nagaram")) # true
 print(isAnagram("rat", "car"))         # false
+
+
+# I also really liked the following solution:
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        char_map = [0] * 26
+
+        for i in range(len(s)):
+            char_map[ord(s[i]) - ord('a')] += 1
+            char_map[ord(t[i]) - ord('a')] -= 1
+
+        return all(count == 0 for count in char_map)
+        
+# the explanation available on:
+#
+# https://leetcode.com/problems/valid-anagram/solutions/
+# 4410440/beats-100-explained-with-video-hashing-c-java-
+# python-js-visualized/
