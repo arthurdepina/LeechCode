4bceae73dc6953e803c8fb4c8666077c36960f7b
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-18 23:42:43 -0300

    renaming

---
+++ leet0021.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLists(list1, list2):
+    tail = dummy = ListNode()  # Por que é necessário criar o dummy?
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            # Não entendi por que a linha acima é assim.
+            # Por que não tail.val = list1.val
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+    
+    tail.next = list1 if list1 else list2
+
+    return dummy.next # Por que retornar dummy e não tail?
--- tests.py
+++ tests.py
@@ -1,9 +1,2 @@
-array = [2, 4, 6, 8, 10]
-for i, j in enumerate(array):
-    print(i, j)
-
-remove_on_index = 2
-new_array = [j for i, j in enumerate(array) if i != remove_on_index]
-# new_array is composed of items (j) in the array if i's index (i) is
-# different from theirs of the item to be removed (i != remove_on_idex)
-print(new_array)
+array = []
+print("[" not in array)
