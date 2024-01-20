32bf0d903e44f15e426f2beb8327794ac072f123
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-20 12:12:20 -0300

    Update leet0058.py

--- leet0058.py
+++ leet0058.py
@@ -7,3 +7,18 @@ def lengthOfLastWord(s) -> int:
 
 print(lengthOfLastWord("luffy is still joyboy"))
 
+
+# A resposta abaixo também é interessante e
+# deve ser mais rápida. Bom para não depender
+# de métodos específicos disponíveis no Python
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        i = len(s) - 1
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+        return length
