5aef03e643a3826cf796f189aa749aa29007f75e
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-21 19:02:40 -0300

    Update leet0066.py
    oops

--- leet0066.py
+++ leet0066.py
@@ -1,13 +1,20 @@
 # 66. Plus One
 
-def plusOne(digits) -> int:
-    for i in range(len(digits) - 1, -1, -1):
-        if digits[i] < 9:
-            digits[i] += 1
-            return digits
-        digits[i] = 0
+def plusOne(digits) -> list[int]:
+    digits[-1] += 1
+    if digits[-1] < 10: return digits
+
+    counter = len(digits) - 1 
+    while digits[counter] >= 10:
+        if counter > 0:
+            digits[counter - 1] += 1
+            digits[counter] -= 10
+            counter -= 1
+        else:
+            digits.insert(0, 1)
+            digits[counter + 1] -= 10
+            break
     
-    digits.insert(0, 1)
     return digits
 
 
