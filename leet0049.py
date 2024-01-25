c8dded213ff59850a91ea606eb96d4f4533b9c6f
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-25 16:03:40 -0300

    leet49

--- leet0049.py
+++ leet0049.py
@@ -1,18 +1,20 @@
 # 49. Group Anagrams
 
+from collections import defaultdict
 
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
-    # strs = sorted(strs)
-    anagrams = dict()
+    anagrams = defaultdict(list)
+
     for word in strs:
-        s_word = ''.join(sorted(word))
-        if s_word in list(anagrams.keys()):
-            anagrams[s_word] += [word]
-        else:
-            anagrams[s_word] = [word]
-    return list(anagrams.values())
+        count = [0] * 26
+        for letter in word:
+            count[ord(letter) - ord('a')] += 1
+        anagrams[tuple(count)].append(word)
+    
+    return anagrams.values()
 
 
-print(groupAnagrams(["eat","tea","tan","ate","nat","bat"])) # [["bat"],["nat","tan"],["ate","eat","tea"]]
-# print(groupAnagrams([""]))                                  # [[""]]
-# print(groupAnagrams(["a"]))                                 # [["a"]]
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+                 # [["bat"],["nat","tan"],["ate","eat","tea"]]
+# print(groupAnagrams([""]))                         #  [[""]]
+# print(groupAnagrams(["a"]))                        # [["a"]]
