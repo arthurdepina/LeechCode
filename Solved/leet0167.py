9c030b3949097c5377b6ba6b601518a17ba2fabb
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-30 22:18:57 -0300

    Update leet0167.py

--- Solved/leet0167.py
+++ Solved/leet0167.py
@@ -8,6 +8,20 @@ def twoSum(numbers: list[int], target: list[int]) -> list[int]:
                 return [i + 1, j + 1]
 
 
-print(twoSum([2,7,11,15], 9)) # [1,2]
-print(twoSum([2,3,4], 6))     # [1,3]
-print(twoSum([-1,0], -1))     # [1,2]
+# print(twoSum([2,7,11,15], 9)) # [1,2]
+# print(twoSum([2,3,4], 6))     # [1,3]
+# print(twoSum([-1,0], -1))     # [1,2]
+
+
+def twoSumTwoPointer(numbers: list[int], target: list[int]) -> list[int]: # 84%
+    a = 0
+    b = len(numbers) - 1
+    while a < b:
+        if numbers[a] + numbers[b] <  target: a += 1
+        if numbers[a] + numbers[b] >  target: b -= 1
+        if numbers[a] + numbers[b] == target: return [a + 1, b + 1]
+
+
+print(twoSumTwoPointer([2,7,11,15], 9)) # [1,2]
+print(twoSumTwoPointer([2,3,4], 6))     # [1,3]
+print(twoSumTwoPointer([-1,0], -1))     # [1,2]
