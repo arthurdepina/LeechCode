e8e8e9c90f679993dce33497103ae6d0e8b150a8
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-02-01 03:04:27 -0300

    works, but too slow.

--- Solved/leet0015.py
+++ Solved/leet0015.py
@@ -3,17 +3,32 @@
 def threeSum(nums: list[int]) -> list[list[int]]:
     output = []
     nums = sorted(nums)
-    for k in range(len(nums)):
-        if k != 0 and nums[k] == nums[k - 1]: continue
-        for i in range(k + 1, len(nums)):
-            if i != k + 1 and nums[i] == nums[i - 1]: continue
-            for j in range(i + 1, len(nums)):
-                if j != i + 1 and nums[j] == nums[j - 1]: continue
-                if nums[k] + nums[i] + nums[j] == 0: output.append([nums[k], nums[i], nums[j]])
+    for a in range(len(nums)):
+        if a != 0 and nums[a] == nums[a - 1]: continue
+        b = a + 1
+        c = len(nums) - 1
+        while b < c:
+            if b != a + 1 and nums[b] == nums[b - 1]:
+                b += 1
+                continue
+            if c != len(nums) - 1 and nums[c] == nums[c + 1]:
+                c -= 1
+                continue
+            if nums[a] + nums[b] + nums[c] == 0:
+                output.append([nums[a], nums[b], nums[c]])
+                if b != len(nums) and c != 0:
+                    b += 1
+                    c -= 1
+                else:
+                    break 
+            if nums[a] + nums[b] + nums[c] < 0: b += 1
+            if nums[a] + nums[b] + nums[c] > 0: c -= 1
     return output
 
-
+ 
 print(threeSum([-1,0,1,2,-1,-4])) # [[-1,-1,2],[-1,0,1]]
 print(threeSum([0,1,1]))          # []
 print(threeSum([0,0,0]))          # [[0,0,0]]
 print(threeSum([0,0,0,0]))        # [[0,0,0]]
+print(threeSum([-2,0,0,2,2]))     # [[-2,0,2]]
+print(threeSum([-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0]))
