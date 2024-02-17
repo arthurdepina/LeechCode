e1f0033e539d1f1179ec347b0e567e6af72aa4f2
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-02-16 23:21:40 -0300

    Update leet0011.py

--- leet0011.py
+++ leet0011.py
@@ -2,21 +2,17 @@
 
 
 def maxArea(height: list[int]) -> int:
-    i = 0
-    min_height = min(height[i], height[- 1])
-    max_area = min_height * (len(height) - 1 - i)
-    while i < len(height) - 1:
-        for j in range(len(height) - 1, i, -1):
-            if height[j] < min_height:
-                continue
-            cur_min_height = min(height[i], height[j])
-            cur_area       = cur_min_height * (j - i)
-            if max(max_area, cur_area) == cur_area:
-                max_area   = cur_area
-                min_height = cur_min_height
-        i += 1
-    return max_area
+    area, l, r = 0, 0, len(height) - 1
 
+    while l < r:
+        area = max(area, min(height[l], height[r]) * (r - l))
+
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+    
+    return area
 
 print(maxArea([1,8,6,2,5,4,8,3,7])) # 49
 print(maxArea([1,1]))               #  1
