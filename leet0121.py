02d25dc0455183dfd6d2f3a07036f949bfa375ad
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-02-26 23:44:33 -0300

    Update leet0121.py

--- leet0121.py
+++ leet0121.py
@@ -3,26 +3,24 @@
 
 def maxProfit(prices: list[int]) -> int:
     if len(prices) < 2: return 0
-    cheap_buy  = prices[0]
-    max_profit = 0
-    for i in range(len(prices) - 1):
-        buy = prices[i]
-        if i != 0 and buy >= cheap_buy:
-            continue
+    
+    left   = 0 # buy
+    right  = 1 # sell
+    profit = 0
+    
+    while right < len(prices) and left <= len(prices) - 1:
+        buy    = prices[left]
+        sell   = prices[right]
+        profit = max(profit, sell - buy)
+        
+        if sell < buy:
+            left = right
+            right += 1
         else:
-            cheap_buy = buy
-            expen_sell = prices[i + 1]
-        for j in range(i, len(prices)):
-            sell = prices[j]
-            if j != 1 and sell < expen_sell:
-                continue
-            else:
-                expen_sell = sell
-            if expen_sell <= cheap_buy:
-                continue
-            else:
-                max_profit = max(max_profit, sell - buy)
-    return max_profit
+            right += 1
+
+    return profit       
+        
 
 
 print(maxProfit([7,1,5,3,6,4])) # 5
